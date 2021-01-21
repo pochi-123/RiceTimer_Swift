3224b14d939a2933812df92d75351b23b2664f5c
akane <dog.house.pochi@gmail.com>
2021-01-21 13:51:44 +0900

    プロジェクト作成

---
+++ RiceTimer_Swift/RiceTimer_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RiceTimer_Swift
+//
+//  Created by Akane Yasuda on 2021/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
